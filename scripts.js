325b66a9275264886df3877d5e6212f29566fbf7
Mikayla Leigh <mikayla.ingham@my.jcu.edu.au>
2017-05-12 20:13:08 +1000

    Completed Indexpage with JS

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function aboutExpand() {
+    var x = document.getElementById('navaboutexp');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function concertsExpand() {
+    var x = document.getElementById('navconcertsexp');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
